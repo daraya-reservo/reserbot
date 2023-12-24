ae1fcc33d488182d42ce29285a93d136041cf903
daraya-reservo <104777673+daraya-reservo@users.noreply.github.com>
2023-12-24 00:37:14 -0300

    a

--- flask_app.py
+++ flask_app.py
@@ -36,7 +36,7 @@ def message_event(data):
     elif message == 'get-newsletter':
         slack_client.post_message(
             channel=event['channel'],
-            text='',
+            text='a',
             btn_text='Leer newsletter',
             url='settings.URL_NOTION_NEWSLETTERS'
         )
