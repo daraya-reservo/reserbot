90a5e70d97e6a92c36e046e863d288075ffb8f4a
daraya-reservo <104777673+daraya-reservo@users.noreply.github.com>
2024-01-19 01:27:27 -0300

    minor fixes

--- schedule.py
+++ schedule.py
@@ -5,13 +5,12 @@ import utils
 
 
 today = datetime.now()
-lider_daily = utils.lider_daily()
-daily_leader = utils.get_daily_leader()
+lider_daily = None
 print('------------PROBANDO FUNCION DAILY NUEVA-------------')
 print('today: ', today.strftime('%A %d'))
-print('lider daily primera forma: ', lider_daily)
+print('is working day?: ', utils.is_working_day(today))
 print('team antes: ', utils.get_team() )
-print('lider daily segunda forma: ', daily_leader)
+print('lider daily: ', utils.get_daily_leader())
 print('team despues: ', utils.get_team() )
 print('------------PROBANDO FUNCION DAILY NUEVA-------------')
 
@@ -20,11 +19,6 @@ if lider_daily:
         channel='#reserbot-shhhh',
         post_at=(today.replace(hour=9, minute=35, second=0)).strftime('%s'),
         text=f'Hoy {today.strftime("%A %d")} lidera {daily_leader} :rubyrun:'
-    )
-    slack_client.schedule(
-        channel='#reservo-ti',
-        post_at=(today.replace(hour=9, minute=35, second=0)).strftime('%s'),
-        text=f'Hoy {today.strftime("%A %d")} lidera {lider_daily} :finoseñores:',
     )
     buttons = [
         {
@@ -37,27 +31,17 @@ if lider_daily:
         }
     ]
     slack_client.schedule(
-        channel='#reservo-ti',
-        post_at=(today.replace(hour=9, minute=50, second=0)).strftime('%s'),
+        channel='#reserbot-shhhh',
+        post_at=(today.replace(hour=9, minute=10, second=0)).strftime('%s'),
         buttons=buttons,
     )
-
-if today.weekday() == 0:
-    first_workday = today if utils.is_working_day(today) else today + timedelta(days=1) 
-    slack_client.schedule(
-        channel='#reserbot-shhhh',
-        post_at=(first_workday.replace(hour=10, minute=30, second=0)).strftime('%s'),
-        buttons=[{
-            'text': 'Leer newsletter',
-            'url': settings.URL_NEWSLETTERS
-        }]
-    )
-else:
-    slack_client.schedule(
-        channel='#reserbot-shhhh',
-        post_at=(today.replace(hour=10, minute=30, second=0)).strftime('%s'),
-        buttons=[{
-            'text': f'Hoy {today.strftime("%s")} no es lunes ni martes',
-            'url': settings.URL_NEWSLETTERS
-        }]
-    )
+    if today.weekday() == 0:
+        first_workday = today if utils.is_working_day(today) else today + timedelta(days=1) 
+        slack_client.schedule(
+            channel='#reserbot-shhhh',
+            post_at=(first_workday.replace(hour=10, minute=30, second=0)).strftime('%s'),
+            buttons=[{
+                'text': 'Leer newsletter',
+                'url': settings.URL_NEWSLETTERS
+            }]
+        )
--- utils.py
+++ utils.py
@@ -1,21 +1,4 @@
 from datetime import datetime
-import locale
-import os
-import pandas
-import pytz
-
-
-def lider_daily():
-    project_path = os.path.realpath(os.path.dirname(__file__))
-    excel_path = f'{project_path}/dailies.xlsx'
-    excel_dailies = pandas.read_excel(excel_path, sheet_name='Hoja1')
-    locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
-    today = datetime.now(pytz.timezone('America/Santiago')).strftime('%Y-%m-%d')
-    lider_daily = excel_dailies[excel_dailies['Día'] == today]['Responsable'].values
-    return lider_daily[0] if lider_daily.size else None
-
-
-from datetime import datetime
 import json
 import locale
 import operator
