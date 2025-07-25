2ce8b6ca5f4676f315b3fd63430efe5c65b5ad2f
daraya-reservo <104777673+daraya-reservo@users.noreply.github.com>
2025-07-25 01:24:46 -0400

    actualizo nombre json

--- team_manager.py
+++ team_manager.py
@@ -6,41 +6,39 @@ import json
 import random
 
 
-TEAM_MEMBERS_FILE = f'{settings.PROJECT_ROOT}/team_members.json'
+MEMBERS_JSON = f'{settings.PROJECT_ROOT}/members.json'
 
 class TeamManager:
     """Class to manage team members."""
     def __init__(self):
-        with open(TEAM_MEMBERS_FILE) as team_members_file:
-            self.members = json.load(team_members_file)
+        with open(MEMBERS_JSON) as members_json:
+            self.members = json.load(members_json)
         self.random_pool = self.members.copy()
 
-    def save(self):
-        with open(TEAM_MEMBERS_FILE, 'w') as team_members_file:
-            json.dump(self.members, team_members_file, indent=4)
+    def save_members(self):
+        with open(MEMBERS_JSON, 'w') as members_json:
+            json.dump(self.members, members_json, indent=4)
 
     def get_unavailable_members(self, day):
-        unavailable_members = {
-            member['name'] for member in self.members if not member['is_available']
-        }
+        unavailable_members = {member['name'] for member in self.members if not member['is_available']}
         # Include Hiho on Fridays (unavailable for today)
         if day.weekday() == 4:
             unavailable_members.add('Hiho')
         return unavailable_members
 
     def get_daily_leader(self, day):
-        team_members = [member for member in self.members if member['is_available']]
+        members = [member for member in self.members if member['is_available']]
         # Exclude Hiho on Fridays
         if day.weekday() == 4:
-            team_members = [member for member in team_members if member['name'] != 'Hiho']
+            members = [member for member in members if member['name'] != 'Hiho']
         # Select the member with the lowest number of dailies
-        leader = min(team_members, key=lambda member: member['dailies'])
+        leader = min(members, key=lambda member: member['dailies'])
         # Update the dailies count for the leader
         for member in self.members:
             if member['tag'] == leader['tag']:
                 member['dailies'] += 1
                 break
-        self.save()
+        self.save_members()
         return leader['tag']
 
     def get_random_leader(self, day):
@@ -59,5 +57,5 @@ class TeamManager:
         for member in self.members:
             if member['tag'] == member_tag:
                 member['is_available'] = available
-        self.save()
+        self.save_members()
 
